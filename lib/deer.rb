245c00da6b7d4a93b7d1ea53ec58fe8c150cef84
andrustory <jc1993box@hotmail.com>
2020-05-25 01:29:00 -0400

    first commit

---
+++ lib/deer.rb
@@ -0,0 +1,11 @@
+
+# environment file
+# in here  loads all files needed to run our app
+
+
+require "deer/version"
+
+module Deer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/deer/version.rb
@@ -0,0 +1,3 @@
+module Deer
+  VERSION = "1.0.0"
+end
