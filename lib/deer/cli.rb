c804c9d120b1242aa5738c3237f46bc132572f78
andrustory <jc1993box@hotmail.com>
2020-05-25 02:14:40 -0400

    add notes for cli class

--- lib/deer/cli.rb
+++ lib/deer/cli.rb
@@ -1,6 +1,29 @@
 class Deer::CLI
+
+    #start should greet user, maybe give a description
+    #get data from scraper/api file
+    #create new custom objects
+    #all inside start method
+
     def start
         puts "Hello there!"
-        binding.pry
+        puts "GETTING DATA FROM API .. PLEASE WAIT"
+        puts "CREATING NEW OBJCS"
     end
+
+    #deal with inputs(loop to keep asking to get new info)
+    #For exmaple   while input != (not equal) "exit"  do  and execute next:
+    #display a list of something, or give exanples of what we expect as input
+    #get user input
+    #depending on what we get, do something
+    #condition to check input for good value
+    #else tell them try again
+
+    #exit command
+    #if input == "EXIT"
+    #kill program, say good bye
+
+
+
+    
 end
